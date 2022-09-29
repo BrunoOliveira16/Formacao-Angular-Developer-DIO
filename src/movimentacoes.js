a57432d92f358fe08636d691b99dcdf394e3681e
Bruno <bruno1_oliveira@hotmail.com>
2022-09-29 20:33:30 -0300

    Atualizacao aula regex, replace e match

--- src/movimentacoes.js
+++ src/movimentacoes.js
@@ -22,18 +22,26 @@ class Registro {
 
     resumo(){
         let valorAtualizado = 0
+        let tipo = ''
+        const regex = new RegExp('x', 'gi') // Expressões Regulares
         this.movimentacoes.forEach(lancamento => {
-            valorAtualizado += lancamento.saldo
+            lancamento.banco = lancamento.banco.replace(regex, 'Y')
+            if(!lancamento.banco.match(regex)){    //regex para filtrar o tipo de movimentação
+                valorAtualizado += lancamento.saldo
+                tipo += lancamento.nome + ' | '
+            }
+           
         })
-        return valorAtualizado
+        return tipo + valorAtualizado
     }
 }
 
-const m01 = new Movimentacao('Modal', 'salario', 10000)
-const m02 = new Movimentacao('Modal', 'comissao', 1000)
-const m03 = new Movimentacao('Modal', 'imposto', -3000)
-const m04 = new Movimentacao('Modal', 'contas', -2000)
+const m01 = new Movimentacao('Bank X', 'salario', 10000)
+const m02 = new Movimentacao('Bank Z', 'comissao', 1000)
+const m03 = new Movimentacao('Bank Y', 'imposto', -3000)
+const m04 = new Movimentacao('Bank X', 'contas', -2000)
+const m05 = new Movimentacao('Bank Y', 'custo', -1000)
 
 const registro01 = new Registro(22,10,2021)
-registro01.novaMovimentacao(m01, m02, m03, m04)
-console.log('O saldo de registros foi de: ' + registro01.resumo())
+registro01.novaMovimentacao(m01, m02, m03, m04, m05)
+console.log('O saldo de registros ' + registro01.resumo())
