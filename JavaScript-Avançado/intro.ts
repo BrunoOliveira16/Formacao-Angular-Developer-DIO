16be70b8dfa20c69f52e1dc381fdde051aaf3a39
Bruno <bruno1_oliveira@hotmail.com>
2022-10-07 20:32:23 -0300

    Atualizacao introducao ao typescript

---
+++ JavaScript-Avançado/intro.ts
@@ -0,0 +1,12 @@
+// Tuple
+let title: [number, string]
+title: ["1", "Título"]
+
+// enum {chave: valor}
+enum Color {
+    white = '#fff',
+    red = '#f00',
+    blue = '#00f'
+}
+
+// Comando utilizado na aula npx tsc ./JavaScript-Avançado/intro.ts
